f6029e55ef5337a3df41f602e00bf323c92f23cb
CaioLLima <caiololima2@gmail.com>
2025-04-22 20:39:32 -0300

    Criado classe ContaCorrente com seus atributos, e alguams funções.

--- ContaCorrente/Program.cs
+++ ContaCorrente/Program.cs
@@ -1,5 +1,42 @@
-﻿namespace ContaCorrente
+﻿using System.Drawing;
+
+namespace ContaCorrente
 {
+    public class ContaCorrente
+    {
+        public int id;
+        public double saldoDisponivel;
+        public double limiteDebito;
+
+        public double Saque(int valor)
+        {
+            double resultado = limiteDebito -= valor;
+            return resultado;
+        }
+        public double Deposito(int valor)
+        {
+            double resultado = limiteDebito += valor;
+            return resultado;
+        }
+        public double Consulta()
+        {
+            return saldoDisponivel;
+        }
+        public double Extrato()
+        {
+            return 10;
+        }
+        public void TransferirPara(ContaCorrente conta, int valor)
+        {
+            conta.saldoDisponivel = + valor;
+        }
+
+        public double Movimentacoes()
+        {
+            return 10;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
